f9f8101ff018cca7e1aeec09afc587c0fa27cf93
wangqianqiong <786925436@qq.com>
2026-05-12 00:51:45 +0800

    Upload 停止系统.bat

--- 停止系统.bat
+++ 停止系统.bat
@@ -2,8 +2,8 @@
 chcp 65001 >nul
 cd /d C:\Users\Administrator\chaonan-yuju
 echo 正在停止...
-pm2 stop chaonan-yuju 2>nul
-pm2 delete chaonan-yuju 2>nul
+taskkill /f /im node.exe 2>nul
+taskkill /f /im ngrok.exe 2>nul
 echo 已停止
 timeout /t 2 /nobreak >nul
 exit
